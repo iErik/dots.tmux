2d8834762bc3bcfe013e2fb8b36c82a8a9bafeb1
Erik Isidore <ericssonico@protonmail.ch>
2026-04-02 01:47:53 -0300

    Migrate to HM completely

--- nix/default.nix
+++ nix/default.nix
@@ -58,14 +58,13 @@ in {
     programs.tmux = {
       enable = true;
       tmuxinator.enable = cfg.tmuxinator;
+
       terminal = "tmux-256color";
       escapeTime = 20;
+      keyMode = "vi";
+      mouse = true;
 
-      extraConfig = ''
-        set -as terminal-overrides ',*:Tc'
-        set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
-        set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
-      '';
+      extraConfig = builtins.readFile ../tmux.base.conf;
 
       plugins = [
         {
