c66344a24e053fc7a03a633817444d4f6b20dd1b
Erik Isidore <ericssonico@protonmail.ch>
2026-05-01 04:40:23 +0000

    Nix: something weird...

--- nix/default.nix
+++ nix/default.nix
@@ -64,7 +64,8 @@ in {
       keyMode = "vi";
       mouse = true;
 
-      extraConfig = builtins.readFile ../tmux.base.conf;
+      #extraConfig = builtins.readFile ../tmux.base.conf;
+      extraConfig = builtins.readFile ../tmux.conf;
 
       plugins = [
         {
